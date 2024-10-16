a2f77615e261ec74d982f1259dd1c8824db063af
tobias28ti <cespedestobias133@gmail.com>
2024-10-16 14:00:13 -0300

    arreglando y agregando punto 3 tobias

--- tarea-programacion-3er-punto.c
+++ tarea-programacion-3er-punto.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
 // Procedimiento para repetir un texto una X cantidad de veces
-void repetir(char *texto, int veces) {
-    for(int i = 0; i < veces; i++) {
+void repetir(char texto[], int veces) {
+    int i;
+	for(i = 0; i < veces; i++) {
         printf("%s\n", texto);
     }
 }
 
 // Procedimiento que usa el procedimiento repetir y rodea el texto con líneas punteadas
-void cartel(char *texto, int veces) {
+void cartel(char texto[], int veces) {
     printf("----------------------------\n");  // Línea punteada antes
     repetir(texto, veces);               // Llamada a repetir para imprimir el texto
     printf("----------------------------\n");  // Línea punteada después
@@ -16,7 +17,11 @@ void cartel(char *texto, int veces) {
 
 int main() {
     char texto[] = "Hola, mundo!";
-    int veces = 3;
+    char texto2[] = "Juan";
+	int veces = 3;
+    
+    // Llamada al procedimiento repetir
+    repetir(texto2,veces);
     
     // Llamada al procedimiento cartel
     cartel(texto, veces);
