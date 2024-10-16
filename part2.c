124dce70c9584c98e531afd6f6fb10cd46ad7886
soofiixd <sofiavdapalma@gmail.com>
2024-10-16 14:25:31 -0300

    corrigiendo codigo

--- part2.c
+++ part2.c
@@ -11,17 +11,9 @@ float pul_a_cm(float pulgadas) {
 }
 
 int main() {
-    float cm, pulgadas;
+    float pulgadas=80;
 
-    // indicar la cantidad de centimetros al usuario
-    printf("ingrese cantidad de centimetros a convertir a pulgadas:\n");
-    scanf("%f", &cm);
     printf("%.2f cm son %.2f pulgadas\n", cm, cm_a_pul(cm));
-
-    // indicar la cantidad de pulgadas al usuario
-    printf("ingrese cantidad de pulgadas a convertir a centimetros:\n");
-    scanf("%f", &pulgadas);
-    printf("%.2f pulgadas son %.2f cm\n", pulgadas, pul_a_cm(pulgadas));
 
     return 0;
 }
