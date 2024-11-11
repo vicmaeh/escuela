c29874123a0d3e059af18404cf19122b26706acf
vicmaeh <lmaehara1954@gmail.com>
2024-11-10 23:26:39 -0300

    Agregando nombre del programador y links (personal y grupal)

--- parte4.c
+++ parte4.c
@@ -1,7 +1,12 @@
 // Procedimiento 4 - Procedimiento "Linea" : mostrar� por pantalla una l�nea de 20 caracteres con el simbolo indicado.
 // Victor Maehara
 
+
 #include <stdio.h>
+
+// Código del alumno Victor Maehara
+// Link de GIT personal: https://github.com/vicmaeh
+// Link del GIT grupal (lider Victor Maehara): https://github.com/vicmaeh/escuela-prueba
 
 void linea(char simbolo){ // Declarando el procedimiento "linea" que admite un carácter, el cual será repetido 20 veces para formar la misma.
 	for(int x=0;x < 20; x++){
